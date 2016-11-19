a45ea2423f8ac5edf38b5209309f33461476a125
Henry Snoek <snoek09@gmail.com>
2016-11-19 17:47:56 +0100

    split up controller tests

---
+++ src/AppBundle/Tests/Controller/User/DetailsControllerTest.php
@@ -0,0 +1,30 @@
+<?php
+namespace AppBundle\Tests\Controller\User;
+
+use GuzzleHttp\Client;
+use Symfony\Component\HttpFoundation\Response;
+
+final class ListControllerTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var Client
+     */
+    private $client;
+
+    public function setUp()
+    {
+        $this->client = new Client([
+            'base_uri' => 'http://localhost:8000',
+            'http_errors' => false,
+        ]);
+    }
+
+    public function testGet()
+    {
+        $userId = 1;
+        $response = $this->client->get('/users/' . $userId);
+
+        $this->assertEquals(Response::HTTP_OK, $response->getStatusCode());
+        $this->assertNotEmpty($response->getBody());
+    }
+}
